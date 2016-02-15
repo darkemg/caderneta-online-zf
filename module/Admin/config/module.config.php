57004a7c3a5bac84b0c7c5dfee19e226670905cd
darkemg <darkemg@users.noreply.github.com>
2016-02-15 02:29:33 -0200

    Teste com a configuração de roteamento.

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -2,8 +2,33 @@
 return [
 	'controllers' => [
 		'invokables' => [
-			'Admin\Controller\Aadmin' => 'Admin\Controller\AdminController',
+			'Admin\Controller\Admin' => 'Admin\Controller\AdminController',
+			'Admin\Controller\Login' => 'Admin\Controller\LoginController'
 		],
+	],
+	'router' => [
+		'routes' => [
+			'admin' => [
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => [
+					'route' => '/admin'
+				],
+				'defaults' => [
+					'controller' => 'Admin\Controller\Admin',
+					'action' => 'index'
+				]
+			],
+			'login' => [
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => [
+					'route' => '/admin/login'
+				],
+				'defaults' => [
+					'controller' => 'Admin\Controller\Login',
+					'action' => 'login'
+				]
+			]
+		]	
 	],
 	'view_manager' => [
 		'template_path_stack' => [
---
+++ module/Admin/src/Admin/Controller/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class LoginController extends AbstractActionController {
+	
+	public function loginAction() {
+		echo "login controller";
+	}
+}
